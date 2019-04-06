f9903e48877c0969b9245404b89c6142d7416cdd
Carden Z <ephemere@protonmail.com>
2019-04-06 20:50:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,4 +8,8 @@ class Patient
   def new_appointment(doctor, date)
     Appointment.new(self, date, doctor)
   end
+
+  def appointments
+    Appointment.all.collect{|a| a.patient == self.name}
+  end
 end
