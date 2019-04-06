3f61cc9806d43c1d34cdf779caf281ff156d4be9
Carden Z <ephemere@protonmail.com>
2019-04-06 20:32:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,11 @@
 class Appointment
   @@all = []
-  attr_reader :date, :patient
+  attr_accessor :date, :patient, :doctor
 
   def initialize(date, patient, doctor)
     @date    = date
     @patient = patient
+    @doctor  = doctor
     @@all << self
   end
 
